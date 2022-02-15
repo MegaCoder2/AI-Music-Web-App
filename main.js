8723b1c9db4115ef74601d24fa0a518b4a51f37b
Mega <83192893+MegaCoder2@users.noreply.github.com>
2022-02-14 18:45:46 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 song1 = ""
 song2 = ""
+var leftWristX = 0
+var rightWristX = 0
+var leftWristY = 0
+var rightWristY = 0
+var scoreleft = 0
+var scoreright = 0
 function preload()
 {
     song1 = loadSound("music.mp3")
     song2 = loadSound("Peter-Pan-and-the-Pirates.mp3")
+}
+function getPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results)
+        leftWristX = results[0].pose.leftWrist.x
+        rightWristX = results[0].pose.rightWrist.x
+        leftWristY = results[0].pose.leftWrist.y
+        rightWristY = results[0].pose.rightWrist.y
+        scoreleft = Math.round(results[0].pose.keypoints[9].score * 100)
+        scoreright = Math.round(results[0].pose.keypoints[10].score * 100)
+        console.log("Left wrist (X, Y): " + leftWristX + ", " + leftWristY)
+        console.log("Right wrist (X, Y): " + rightWristX + ", " + rightWristY)
+        console.log("Score left: " + scoreleft + "%, Score right: " + scoreright + "%")
+    }
+}
+function modelLoaded()
+{
+    console.log("PoseNet loaded successfully.")
 }
 function setup()
 {
@@ -11,6 +37,8 @@ function setup()
     canvas.center()
     video = createCapture(VIDEO)
     video.hide()
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on("pose", getPoses)
 }
 function draw()
 {
